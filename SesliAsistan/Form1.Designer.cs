b6189c087fa94e7176dcd856c098c4d4408e6f3f
Aksel Kurt <akselkurt@hotmail.com>
2019-05-30 10:18:38 +0300

    First commit

---
+++ SesliAsistan/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace SesliAsistan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSpeech = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSpeech
+            // 
+            this.btnSpeech.BackColor = System.Drawing.Color.GreenYellow;
+            this.btnSpeech.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSpeech.Location = new System.Drawing.Point(85, 81);
+            this.btnSpeech.Name = "btnSpeech";
+            this.btnSpeech.Size = new System.Drawing.Size(103, 73);
+            this.btnSpeech.TabIndex = 0;
+            this.btnSpeech.Text = "Speech";
+            this.btnSpeech.UseVisualStyleBackColor = false;
+            this.btnSpeech.Click += new System.EventHandler(this.btnSpeech_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Red;
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(240, 81);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(106, 73);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Exit";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 229);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnSpeech);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSpeech;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ SesliAsistan/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+
+namespace SesliAsistan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SpeechSynthesizer synt = new SpeechSynthesizer();
+        PromptBuilder prompt = new PromptBuilder();
+        SpeechRecognitionEngine engine = new SpeechRecognitionEngine();
+
+        private void btnSpeech_Click(object sender, EventArgs e)
+        {
+            Choices list = new Choices();
+            list.Add(new String[] { "open cassandra", "open chrome","exit" });
+
+            Grammar world = new Grammar(new GrammarBuilder(list));
+            try
+            {
+                engine.RequestRecognizerUpdate();
+                engine.LoadGrammar(world);
+
+                engine.SpeechRecognized += SpeechRecognized;
+                engine.SetInputToDefaultAudioDevice();
+                engine.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch (Exception z)
+            {
+                Console.WriteLine(z);
+                return;
+            }
+        }
+        void SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+
+            string text = e.Result.Text;
+            switch (e.Result.Text)
+            {
+                case "open cassandra":
+                    System.Diagnostics.Process.Start("C:/Users/Phillip/Desktop/Cassandra");
+                    prompt.ClearContent();
+                    prompt.AppendText("Cassandra opened");
+                    synt.Speak(prompt);
+                    break;
+                case "open chrome":
+                    {
+                        System.Diagnostics.Process.Start("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe");
+                        prompt.ClearContent();
+                        prompt.AppendText("Chrome opened");
+                        synt.Speak(prompt);
+                        break;
+                    }
+                case "exit":
+                    prompt.ClearContent();
+                    prompt.AppendText("Exiting");
+                    synt.Speak(prompt);
+                    Application.Exit();
+                    break;
+            }
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            engine.Dispose();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            prompt.ClearContent();
+            prompt.AppendText("Welcome to assistan, please speech button for speech");
+            synt.Speak(prompt);
+        }
+    }
+}
